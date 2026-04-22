3216d29840849629dc1aa74ea5abcde54e2c742a
Jan Guiao <95273542+JanGuiao@users.noreply.github.com>
2026-04-22 00:44:33 -0400

    Update and rename WN11-AU-000586 to WN11-AU-000586.ps1

--- STIGS/WN11-AU-000586.ps1
+++ STIGS/WN11-AU-000586.ps1
@@ -1,3 +1,32 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that Windows 11 enforces a password history of at least 24 passwords, preventing users from reusing recent passwords and strengthening overall account security in compliance with STIG requirements.
+
+.NOTES
+    Author          : Jan Guiao
+    LinkedIn        : linkedin.com/in/JanGuiao/
+    GitHub          : github.com/JanGuiao
+    Date Created    : 2026-04-21
+    Last Modified   : 2026-04-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-WN11-AC-000586
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-AU-000586/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\Users\j4n> C:\Users\j4n\Desktop\WN11-AU-000586.ps1
+#>
+
+# YOUR CODE GOES HERE
 
 # STIG WN11-AU-000586 - Audit Registry (Success)
 
